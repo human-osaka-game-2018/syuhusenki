57da38b600934002c9d401465721b66bd4cfcefa
mituki0929 <39544408+mituki0929@users.noreply.github.com>
2018-08-25 01:05:47 +0900

    タイマーの処理を新たなcpp&hファイルに分離

--- 主婦戦記/syuhusenki/syuhusenki/FloaMove.cpp
+++ 主婦戦記/syuhusenki/syuhusenki/FloaMove.cpp
@@ -1,46 +1,29 @@
 #include "Main.h"
 #include "FloaMove.h"
 #include "GameMain.h"
+#include "Timer.h"
 
 //ヤス子のステータス
-CHARACTER_STATE g_yasukoSta = { 1.f, 1.5f, 1.f };
+CHARACTER_STATE g_yasukoSta = { 1, 1.5f, 1 };
 
 //ミツ子のステータス
-CHARACTER_STATE g_mitukoSta = { 2.f, 1.5f, 1.25 };
+CHARACTER_STATE g_mitukoSta = { 2, 1.5f, 1.25 };
 
 //イソ子のステータス
-CHARACTER_STATE g_isokoSta = { 2.f, 2.f, 1.f };
+CHARACTER_STATE g_isokoSta = { 2, 2.f, 1 };
 
 CENTRAL_STATE g_PCSta = { 900.f, 580.f, 32.f, 53.f };
-CENTRAL_STATE g_timerSta = { 1210.f, 50.f, 50.f, 50.f };
 CENTRAL_STATE g_startCountSta = { 520.f, 350.f, 150.f, 150.f };
 CENTRAL_STATE g_startSta = { 520.f, 350.f, 200.f, 96.25f };
-CENTRAL_STATE g_timeUpSta = { 520.f, 350.f, 260.f, 100.f };
 
 FLOAT g_PCSpeed = 0.f;
-
-int g_gameCount = 0;
 
 //ゲーム制御処理
 VOID floaMoveControl(VOID)
 {
 	static int onceSound = 0;
 
-	if (g_gameCount < 10980)
-	{
-		g_gameCount++;
-	}
-
-	if (g_gameCount == 180)
-	{
-		g_isGameStart = true;
-	}
-
-	if (g_gameCount == 10980)
-	{soundsManager.SetVolume("FOOD", 25);
-		soundsManager.Stop("FOOD");
-		g_isTimeUp = true;
-	}
+	timerControl();
 
 	if (g_isTimeUp)
 	{
@@ -236,60 +219,38 @@ VOID floaMoveRender(VOID)
 //ゲーム画面のテクスチャ
 VOID floaMoveRenderSta(VOID)
 {
-	static float timerRotation = 0.f;
 	EasyCreateSquareVertex(0, 0, WIDTH, HEIGHT, FLOAMOVE_BG_TEX);
 
-	CUSTOMVERTEX timer[4];
-	CUSTOMVERTEX timerHand[4];
 	CUSTOMVERTEX PC[4];
 	CUSTOMVERTEX startCount[4];
 	CUSTOMVERTEX start[4];
-	CUSTOMVERTEX timeUp[4];
 
-	CreateSquareVertex(timer,g_timerSta);
-	CreateSquareVertex(timerHand,g_timerSta);
 	CreateSquareVertex(PC,g_PCSta);
 	CreateSquareVertex(startCount,g_startCountSta);
 	CreateSquareVertex(start, g_startSta);
-	CreateSquareVertex(timeUp, g_timeUpSta);
-
-	//回転
-	RevolveZ(timerHand, timerRotation, g_timerSta);
-
-
-	//タイマーのテクスチャの描画
-	SetUpTexture(timer, TIMER_FRAME_TEX);
-
-	//タイマーのテクスチャの描画
-	SetUpTexture(timerHand, TIMER_HAND_TEX);
 
 	//プレイヤーキャラクターのテクスチャの描画
 	SetUpTexture(PC, texturePC);
 
-	if ((g_gameCount > 0) && (g_gameCount <= 60))
+	if ((g_timerCount > ZERO_SECOND) && (g_timerCount <= ONE_SECOND))
 	{
 		//スタートカウントのテクスチャの描画
 		SetUpTexture(startCount, STARTCOUNT_3_TEX);
 	}
-	else if ((g_gameCount > 60) && (g_gameCount <= 120))
+	else if ((g_timerCount > ONE_SECOND) && (g_timerCount <= TWO_SECOND))
 	{
 		//スタートカウントのテクスチャの描画
 		SetUpTexture(startCount, STARTCOUNT_2_TEX);
 	}
-	else if ((g_gameCount > 120) && (g_gameCount <= 180))
+	else if ((g_timerCount > TWO_SECOND) && (g_timerCount <= THREE_SECOND))
 	{
 		//スタートカウントのテクスチャの描画
 		SetUpTexture(startCount, STARTCOUNT_1_TEX);
 	}
-	else if ((g_gameCount > 180) && (g_gameCount <= 240))
+	else if ((g_timerCount > THREE_SECOND) && (g_timerCount <= FOUR_SECOND))
 	{
 		//スタートカウントのテクスチャの描画
 		SetUpTexture(start, START_TEX);
-	}
-
-	if (g_gameCount >= 180 && !g_isTimeUp)
-	{
-		timerRotation += -0.001744;
 	}
 
 	if (g_pause && !(g_isTimeUp))
@@ -297,10 +258,5 @@ VOID floaMoveRenderSta(VOID)
 		EasyCreateSquareVertex(0, 0, WIDTH, HEIGHT, PAUSE_TEX);
 	}
 
-	if (g_isTimeUp)
-	{
-		//タイムアップのテクスチャの描画
-		SetUpTexture(timeUp, TIMEUP_TEX);
-	}
-
+	timerRender();
 }
---
+++ 主婦戦記/syuhusenki/syuhusenki/Timer.h
@@ -0,0 +1,8 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+VOID timerControl(VOID);
+
+VOID timerRender(VOID);
+
+#endif // !TIMER_H
